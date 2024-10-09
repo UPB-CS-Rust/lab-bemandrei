7f2bdce17cb3c866cedc3ba47b034f5bec61e4d9
bemandrei <64781637+bemandrei@users.noreply.github.com>
2024-10-09 16:27:25 +0000

    test

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,14 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
-
-    // TODO
-
-    println!("{} is largest and {} is smallest");
+    let mut x = 0;
+    let mut y = 100;
+    for i in 0..8{
+        if input[i] > x{
+            x = input[i]
+        }
+        if input[i] < y{
+            y = input[i]
+        }
+    }
+    println!("{} is largest and {} is smallest", y, x);
 }
